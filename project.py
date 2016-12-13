fe3611911bcac2d7ba6c021e9a31b893be8136b9
Chen Ding <sinochen1992@gmail.com>
2016-12-13 11:49:44 -0500

    version 0.2

--- project.py
+++ project.py
@@ -14,10 +14,18 @@ def f(r,t):
     ax = -x/(x**2+y**2+z**2)**(3.0/2)
     ay = -y/(x**2+y**2+z**2)**(3.0/2)
     az = -z/(x**2+y**2+z**2)**(3.0/2)
-    return np.array([vx,vy,vz,ax,ay,az],float)
+    xA = r[6]
+    yA = r[7]
+    zA = r[8]
+    vxA = r[9]
+    vyA = r[10]
+    vzA = r[11]
+    axA = -xA/(xA**2+yA**2+zA**2)**(3.0/2) - GMJ/GM *(xA-x)/((xA-x)**2+(yA-y)**2+(zA-z)**2)**(3.0/2)
+    ayA = -yA/(xA**2+yA**2+zA**2)**(3.0/2) - GMJ/GM *(yA-y)/((xA-x)**2+(yA-y)**2+(zA-z)**2)**(3.0/2)
+    azA = -zA/(xA**2+yA**2+zA**2)**(3.0/2) - GMJ/GM *(zA-z)/((xA-x)**2+(yA-y)**2+(zA-z)**2)**(3.0/2)
+    return np.array([vx,vy,vz,ax,ay,az,vxA,vyA,vzA,axA,ayA,azA],float)
     
   
-
 def AdaptiveRK4(start, end, totalaccuracy, r):
     
     def timestep(t0,r0,h,step_count):
@@ -43,10 +51,9 @@ def AdaptiveRK4(start, end, totalaccuracy, r):
         k4 = 2*h*f(r0+k3,t0+h*2)
         r2 = r0 + (k1+2*k2+2*k3+k4)/6
         
-        errorx = (r1[0]-r2[0])/30
-        errory = (r1[1]-r2[1])/30
-        errorz = (r1[2]-r2[2])/30
-        error = np.sqrt(errorx**2+errory**2+errorz**2)
+        error2J = ((r1[0]-r2[0])/30)**2 + ((r1[1]-r2[1])/30)**2 + ((r1[2]-r2[2])/30)**2
+        error2A = ((r1[6]-r2[6])/30)**2 + ((r1[7]-r2[7])/30)**2 + ((r1[8]-r2[8])/30)**2
+        error = np.sqrt(error2J+error2A)
         rho = h * precision / error
         
         if rho >= 1:
@@ -57,6 +64,13 @@ def AdaptiveRK4(start, end, totalaccuracy, r):
             xpoints.append(r0[0])
             ypoints.append(r0[1])
             zpoints.append(r0[2])
+            tanJ = r0[1]/r0[0]
+            tanA = r0[7]/r0[6]
+            tanJA = (tanJ-tanA)/(1+tanJ*tanA)
+            anglepoints.append(np.arctan(tanJA)-np.pi/3)
+            radiusJA = np.sqrt(r0[6]**2+r0[7]**2)-np.sqrt(r0[0]**2+r0[1]**2)
+            radiuspoints.append(radiusJA)
+            zApoints.append(r0[8])
             Epoints.append(0.5*(r0[3]**2+r0[4]**2+r0[5]**2)-1.0/np.sqrt(r0[0]**2+r0[1]**2+r0[2]**2))
             if rho**(1/4)<2:
                 h = h * rho**(1/4)
@@ -70,37 +84,51 @@ def AdaptiveRK4(start, end, totalaccuracy, r):
     precision = totalaccuracy/(end-start)
     t0 = start
     r0 = r.copy()
-    h = float(end-start)/ 1000
+    h = float(end-start)/ 10000
     step_count = 0   
     
     tpoints = []
     xpoints = []
     ypoints = []
     zpoints = []
+    anglepoints = []
+    radiuspoints = []
+    zApoints = []
     Epoints = []
+    
     tpoints.append(t0)
     xpoints.append(r0[0])
     ypoints.append(r0[1])
     zpoints.append(r0[2])
+    tanJ = r0[1]/r0[0]
+    tanA = r0[7]/r0[6]
+    tanJA = (tanJ-tanA)/(1+tanJ*tanA)
+    anglepoints.append(np.arctan(tanJA)-np.pi/3)
+    radiusJA = np.sqrt(r0[6]**2+r0[7]**2)-np.sqrt(r0[0]**2+r0[1]**2)
+    radiuspoints.append(radiusJA)
+    zApoints.append(r0[8])
     Epoints.append(0.5*(r0[3]**2+r0[4]**2+r0[5]**2)-1.0/np.sqrt(r0[0]**2+r0[1]**2+r0[2]**2))
     
     while t0 < end:
         t0, r0, h, step_count = timestep(t0 ,r0, h, step_count)
-    return np.array(tpoints), np.array(xpoints), np.array(ypoints), np.array(zpoints),np.array(Epoints), step_count
+    return np.array(tpoints), np.array(xpoints), np.array(ypoints), np.array(zpoints), \
+            np.array(anglepoints), np.array(radiuspoints), np.array(zApoints), np.array(Epoints), step_count
     
         
-# Mars
+# Astronomical Data
     
 GM = 1.327124400189*10**20
-a = 227.9392*10**9 # semi-major axis as unit of length
-e = 0.0934  # eccentricity
+GMJ = 1.266865349*10**17
+a = 778.299*10**9 # semi-major axis as unit of length
+# e = 0.048498  # eccentricity
+e = 0.
 time_unit_in_second = np.sqrt(a**3/GM)
 Julian_year_in_second = 365.25 * 86400
 time_unit_in_years = time_unit_in_second / Julian_year_in_second
 period_in_years = 2*np.pi*time_unit_in_years
-t_final = 5.0 / time_unit_in_years
-r_ini = np.array([1.0+e,0.0,0.0,0.0,np.sqrt((1-e)/(1+e)),0.0])
-
+t_final = 100.0 / time_unit_in_years
+r_ini = np.array([1.+e, 0., 0.,  0., np.sqrt((1-e)/(1+e)), 0.,
+                  (1.+e)*np.cos(np.pi/3), -1*(1.+e)*np.sin(np.pi/3), 0.,  np.sqrt((1-e)/(1+e))*np.sin(np.pi/3), np.sqrt((1-e)/(1+e))*np.cos(np.pi/3), 0.])
 
 # calculate exact solution
 
@@ -115,24 +143,35 @@ x_final_exact = -1 * np.cos(eccentric_anomaly) + e
 y_final_exact = -1 * np.sin(eccentric_anomaly) * np.sqrt(1-e**2)
 
 
+# calculation
 
-tpointsARK4, xpointsARK4, ypointsARK4, zpointsARK4,EpointsARK4, step_count= AdaptiveRK4(0, t_final, 10**(-8), r_ini)
+tpointsARK4, xpointsARK4, ypointsARK4, zpointsARK4, anglepointsARK4, radiuspointsARK4, zApointsARK4, EpointsARK4, step_count= AdaptiveRK4(0, t_final, 10**(-10), r_ini)
 error_final = np.sqrt((x_final_exact - xpointsARK4[-1])**2 + (y_final_exact - ypointsARK4[-1])**2 )
 
 
-# plot of orbits
+# plot of orbits of Jupiter
 fig1 = plt.figure()
 ax1 = fig1.add_subplot(1,1,1)
 ax1.plot(xpointsARK4, ypointsARK4, label='Step Count='+str(step_count))
 ax1.set_xlabel(r'$x/a$')
 ax1.set_ylabel(r'$y/a$')
-plt.title('orbit of Mars using Adaptive RK4')
+plt.title('orbit of Jupiter using Adaptive RK4')
+plt.legend(loc=0)
+plt.show()
+
+# plot of trajectory of asteroid relative to Lagrangian point L5
+fig2 = plt.figure()
+ax1 = fig2.add_subplot(1,1,1)
+ax1.plot(anglepointsARK4, radiuspointsARK4, label='Step Count='+str(step_count))
+ax1.set_xlabel(r'$angle/radian$')
+ax1.set_ylabel(r'$radius/a$')
+plt.title('trajectory of Trojan asteroid relative to Lagrangian points L5')
 plt.legend(loc=0)
 plt.show()
 
 # energy plot
-fig2 = plt.figure()
-ax1 = fig2.add_subplot(1,1,1)
+fig3 = plt.figure()
+ax1 = fig3.add_subplot(1,1,1)
 ax1.plot(tpointsARK4, EpointsARK4, label='Energy')
 ax1.set_xlabel(r'$t/ \sqrt{\frac{a^3}{GM}}$')
 ax1.set_ylabel(r'$E/ \frac{GMm}{a}$')
